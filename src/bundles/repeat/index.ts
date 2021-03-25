f5817debbe07449861382ac4afc681284f624511
Bryan Loh <lamofacade@gmail.com>
2021-03-26 02:22:23 +0800

    Add typedoc for automatic typescript documentation of modules

--- src/bundles/repeat/index.ts
+++ src/bundles/repeat/index.ts
@@ -1,21 +1,10 @@
+import { repeat, twice, thrice } from './functions';
+
 /**
  * Test bundle for Source Academy modules repository
  * @author Loh Xian Ze, Bryan
  * @author Tang Xin Kye, Marcus
  */
-
-// eslint-disable-next-line no-underscore-dangle
-function repeat(f: Function, n: any) {
-  return n === 0 ? (x: any) => x : (x: any) => f(repeat(f, n - 1)(x));
-}
-
-function twice(f: any) {
-  return repeat(f, 2);
-}
-
-function thrice(f: any) {
-  return repeat(f, 3);
-}
 
 export default () => ({
   repeat,
